781ef14c3b39e026557b31cda579623db7c9f464
Pavel Kanzelsberger <pavel@kanzelsberger.com>
2026-09-19 18:20:37 +0200

    Declare Swift 6 for the package deployment targets

--- Package.swift
+++ Package.swift
@@ -1,8 +1,8 @@
-// swift-tools-version:5.7
+// swift-tools-version:6.0
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 import PackageDescription
 let checksum = "31c44c2f1bcd99f353a207a9b1222d0bf956976b3311af52be611a76cc9422c1"
-let version = "26.9.7-mango.1"
+let version = "26.9.7-mango.2"
 let url = "https://github.com/kanzelsberger/matrix-rust-components-swift/releases/download/\(version)/MatrixSDKFFI.xcframework.zip"
 let package = Package(
     name: "MatrixRustSDK",
